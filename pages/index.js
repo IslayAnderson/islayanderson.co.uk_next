f16320d3cd01d22051c57292e6948f01fe7b7d8b
play.js <git@gituser.com>
2022-12-19 13:18:16 +0000

    init project

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Fullstack Developer - Islay Anderson - Liverpool</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+      <h1>islay anderson</h1>
+      </main>
+
+      <footer>
+      </footer>
+
+      
+    </div>
+  )
+}
