fe3d8861f8dc4808d26110f808ffebb83de56bf8
Islay Anderson <mranderson64@live.com>
2022-12-26 20:43:28 +0000

    remove hero post from index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,16 +17,6 @@ export default function Index({ allPosts: { edges }, preview }) {
         <title>{CMS_NAME}</title>
       </Head>
       <Container>
-        {heroPost && (
-          <HeroPost
-            title={heroPost.title}
-            coverImage={heroPost.featuredImage}
-            date={heroPost.date}
-            author={heroPost.author}
-            slug={heroPost.slug}
-            excerpt={heroPost.excerpt}
-          />
-        )}
         {morePosts.length > 0 && <MoreStories posts={morePosts} />}
       </Container>
     </Layout>
