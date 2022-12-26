c240fc397f0f0454769671e7ab4ae101642961af
Islay Anderson <mranderson64@live.com>
2022-12-26 20:49:52 +0000

    next branding

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,8 +6,6 @@ export default function Footer() {
     <footer className="bg-accent-1 border-t border-accent-2">
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
-          
-          </div>
         </div>
       </Container>
     </footer>
