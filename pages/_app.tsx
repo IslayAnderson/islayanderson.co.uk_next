08a689102f85e0461b76d7e3c49f893f888b1f41
Islay <mranderson64@live.com>
2023-01-02 02:33:23 +0000

    install prismjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 //import "bootstrap/dist/js/bootstrap.bundle.min";
 import '../styles/index.css'
+//prismjs
+import "prismjs/prism.js";
+import "prismjs/themes/prism-dark.min.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
